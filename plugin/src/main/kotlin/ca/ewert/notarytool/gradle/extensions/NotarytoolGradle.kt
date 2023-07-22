feea0f7a4c3dea12fcb21fafbbbea475f86a0349
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-07-22 10:51:18 -0700

    Added some properties

--- plugin/src/main/kotlin/ca/ewert/notarytool/gradle/extensions/NotarytoolGradle.kt
+++ plugin/src/main/kotlin/ca/ewert/notarytool/gradle/extensions/NotarytoolGradle.kt
@@ -1,9 +1,24 @@
 package ca.ewert.notarytool.gradle.extensions
 
+import org.gradle.api.provider.Property
+import java.nio.file.Path
+
 /**
- * TODO: Add Comments
+ * Extension used to configure the notarytool-gradle plugin.
+ * Contains the credentials required to interact with the
+ * Notary Web API.
  *
  * Created: 2023-07-21
  * @author Victor Ewert
  */
-interface NotarytoolGradle
+interface NotarytoolGradle {
+
+  /** Issuer ID from the API Keys page in App Store Connect  */
+  val issuerId: Property<String>
+
+  /** The private key ID from App Store Connect */
+  val privateKeyId: Property<String>
+
+  /** Private key file downloaded after creating the App Store Connect API Key */
+  val privateKeyFile: Property<Path>
+}
