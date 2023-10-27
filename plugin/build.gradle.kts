27449859e770127e344ed323eb788ac0b0f005aa
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-10-26 18:39:52 -0700

    Removed unneeded cast.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -53,7 +53,7 @@ java {
 
 kotlin {
   jvmToolchain {
-    (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(11))
+    this.languageVersion.set(JavaLanguageVersion.of(11))
   }
 }
 
