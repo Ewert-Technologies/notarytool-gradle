fa960117c33b9068d9c28269a4abfb5c2cb85e3a
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-11-01 15:00:42 -0700

    Updated dependencyUpdates plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
   `java-gradle-plugin`
   `maven-publish`
   id("org.jetbrains.kotlin.jvm") version "1.9.0"
-  id("com.github.ben-manes.versions") version "0.47.0"
+  id("com.github.ben-manes.versions") version "0.49.0"
   id("org.jmailen.kotlinter") version "3.15.0"
 }
 
