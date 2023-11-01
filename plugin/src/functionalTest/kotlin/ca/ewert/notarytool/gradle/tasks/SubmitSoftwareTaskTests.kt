6b2faa2dca6d70858c8dea81a2ed88d3627774c7
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-11-01 14:09:15 -0700

    Implemented SubmitSoftwareTask and some tests

---
+++ plugin/src/functionalTest/kotlin/ca/ewert/notarytool/gradle/tasks/SubmitSoftwareTaskTests.kt
@@ -0,0 +1,62 @@
+package ca.ewert.notarytool.gradle.tasks
+
+import assertk.assertThat
+import assertk.assertions.contains
+import ca.ewert.notarytool.gradle.readBuildFileContents
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+class SubmitSoftwareTaskTests {
+  @field:TempDir
+  lateinit var projectDir: File
+
+  private val buildFile by lazy { projectDir.resolve("build.gradle.kts") }
+  private val settingsFile by lazy { projectDir.resolve("settings.gradle") }
+
+  @Test
+  fun test1() {
+    settingsFile.writeText("rootProject.name = \"Test-Project\"")
+    val buildFileContents: String = readBuildFileContents("/private/build2.gradle.ktstest")
+    buildFile.writeText(buildFileContents)
+    // Run the build
+    val runner = GradleRunner.create()
+    runner.forwardOutput()
+    runner.withPluginClasspath()
+    runner.withArguments("submitSoftware")
+    runner.withProjectDir(projectDir)
+    val result = runner.build()
+    assertThat(result.output).contains("is not a file.")
+  }
+
+  @Test
+  fun test2() {
+    settingsFile.writeText("rootProject.name = \"Test-Project\"")
+    val buildFileContents: String = readBuildFileContents("/private/build2.gradle.ktstest")
+    buildFile.writeText(buildFileContents)
+    // Run the build
+    val runner = GradleRunner.create()
+    runner.forwardOutput()
+    runner.withPluginClasspath()
+    runner.withArguments("submitSoftware",  "--fileLocation=abc")
+    runner.withProjectDir(projectDir)
+    val result = runner.build()
+    assertThat(result.output).contains("abc does not exist.")
+  }
+
+  @Test
+  fun test3() {
+    settingsFile.writeText("rootProject.name = \"Test-Project\"")
+    val buildFileContents: String = readBuildFileContents("/private/build2.gradle.ktstest")
+    buildFile.writeText(buildFileContents)
+    // Run the build
+    val runner = GradleRunner.create()
+    runner.forwardOutput()
+    runner.withPluginClasspath()
+    runner.withArguments("submitSoftware",  "--fileLocation=/Users/vewert/DevProj/notarytool-gradle/plugin/src/functionalTest/resources/private/pwm_invalid_aarch64.dmg")
+    runner.withProjectDir(projectDir)
+    val result = runner.build()
+    assertThat(result.output).contains("Uploaded file for notarization. Submission ID:")
+  }
+}
--- plugin/src/main/kotlin/ca/ewert/notarytool/gradle/tasks/SubmitSoftwareTask.kt
+++ plugin/src/main/kotlin/ca/ewert/notarytool/gradle/tasks/SubmitSoftwareTask.kt
@@ -1,9 +1,14 @@
 package ca.ewert.notarytool.gradle.tasks
 
 import ca.ewert.notarytoolkotlin.NotaryToolError
+import ca.ewert.notarytoolkotlin.response.AwsUploadData
+import ca.ewert.notarytoolkotlin.response.Status
 import ca.ewert.notarytoolkotlin.response.SubmissionId
-import com.github.michaelbull.result.onFailure
-import com.github.michaelbull.result.onSuccess
+import ca.ewert.notarytoolkotlin.response.SubmissionLogUrlResponse
+import ca.ewert.notarytoolkotlin.response.SubmissionStatusResponse
+import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.fold
+import com.github.michaelbull.result.mapEither
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.options.Option
@@ -12,6 +17,7 @@ import java.nio.file.Path
 import java.time.Duration
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.exists
+import kotlin.io.path.isRegularFile
 
 /**
  * Task for submitting software to be notarized.
@@ -36,53 +42,78 @@ abstract class SubmitSoftwareTask : NotaryToolTask() {
    * Function run when the task in executed.
    */
   override fun taskAction() {
+    logger.lifecycle("Starting task: ${this.name}")
+    logger.debug("fileLocation: ${fileLocation.get()}")
     try {
-      val softwareFilePath = Path.of(fileLocation.get())
+      val softwareFilePath: Path = Path.of(fileLocation.get())
+      logger.debug("softwareFilePath: ${softwareFilePath.absolutePathString()}")
       if (softwareFilePath.exists()) {
-        startSubmission(softwareFile = softwareFilePath)
+        if (softwareFilePath.isRegularFile()) {
+          startSubmission(softwareFile = softwareFilePath)
+        } else {
+          logger.warn("${softwareFilePath.absolutePathString()} is not a file.")
+        }
       } else {
         logger.warn("${softwareFilePath.absolutePathString()} does not exist.")
       }
     } catch (invalidPathException: InvalidPathException) {
       logger.warn("fileLocation is not valid: ${invalidPathException.localizedMessage}")
+    } catch (illegalStateException: IllegalStateException) {
+      logger.warn("Invalid value for 'softwareFilepath'")
     }
   }
 
+  /**
+   * Starts the submission process by uploading the file to the AWS Servers and then uses
+   * the returned submissionId to poll for a status Result.
+   */
   private fun startSubmission(softwareFile: Path) {
-    val submitAndUploadResult = this.client.submitAndUploadSoftware(softwareFile)
-    submitAndUploadResult.onSuccess { awsUploadData ->
-      val submissionId = awsUploadData.submissionId
+    logger.quiet("Submitting file: ${softwareFile.fileName} ...")
+    this.client.submitAndUploadSoftware(softwareFile).fold({ awsUploadData: AwsUploadData ->
+      val submissionId: SubmissionId = awsUploadData.submissionId
       logger.lifecycle("Uploaded file for notarization. Submission ID: ${submissionId.id}")
-    }
-
-    submitAndUploadResult.onFailure { notaryToolError ->
+      pollStatus(submissionId)
+    }, { notaryToolError: NotaryToolError ->
       logger.warn(notaryToolError.longMsg)
-    }
+    })
   }
 
   private fun pollStatus(submissionId: SubmissionId) {
     logger.quiet("Polling status for submission: ${submissionId.id}...")
     val maxPollCount = 50
-    val result = this.client.pollSubmissionStatus(
+    val result: Result<SubmissionStatusResponse, NotaryToolError> = this.client.pollSubmissionStatus(
       submissionId = submissionId,
       maxPollCount = maxPollCount,
       delayFunction = { _: Int -> Duration.ofSeconds(15) },
       progressCallback = { currentPollCount, submissionStatusResponse ->
         logger.quiet(
-          "Checking submission status attempt $currentPollCount of $maxPollCount. Current status: ${submissionStatusResponse.submissionInfo.status}",
+          "Checking submission status, attempt $currentPollCount of $maxPollCount. Current status: ${submissionStatusResponse.submissionInfo.status}",
         )
       },
     )
 
-    result.onSuccess { submissionStatusResponse ->
+    result.fold({ submissionStatusResponse: SubmissionStatusResponse ->
       logger.quiet("Status for submission id ${submissionId.id}: ${submissionStatusResponse.submissionInfo.status}")
-    }
-
-    result.onFailure { notaryToolError ->
+      when (submissionStatusResponse.submissionInfo.status) {
+        Status.ACCEPTED, Status.REJECTED, Status.INVALID -> retrieveSubmissionLogUrl(submissionId)
+        else -> logger.info("No log file")
+      }
+    }, { notaryToolError: NotaryToolError ->
       when (notaryToolError) {
-        is NotaryToolError.PollingTimeout -> logger.warn("Polling timed out. Use 'submissionStatus' task to manually check the status.")
+        is NotaryToolError.PollingTimeout -> logger.warn("Polling timed out. Use 'submissionStatus' task to manually check the status for submission with id: ${submissionId.id}.")
         else -> logger.warn(notaryToolError.longMsg)
       }
-    }
+    })
+  }
+
+  /**
+   * Retrieves and logs the submission log (if available).
+   */
+  private fun retrieveSubmissionLogUrl(submissionIdWrapper: SubmissionId) {
+    this.client.getSubmissionLog(submissionIdWrapper).fold({ submissionLogUrlResponse: SubmissionLogUrlResponse ->
+      logger.quiet("Submission Log: ${submissionLogUrlResponse.developerLogUrlString}")
+    }, { notaryToolError: NotaryToolError ->
+      logger.info("Error getting log: ${notaryToolError.longMsg}")
+    })
   }
 }
