2d323d2ba70c10fdc8a1611ce4758a5b874871be
Victor Ewert <victor.ewert@ewert-technologies.ca>
2024-05-27 15:36:51 -0700

    Added utility function to get a Path from a Resource location

--- plugin/src/functionalTest/kotlin/ca/ewert/notarytool/gradle/TestUtil.kt
+++ plugin/src/functionalTest/kotlin/ca/ewert/notarytool/gradle/TestUtil.kt
@@ -1,5 +1,7 @@
 package ca.ewert.notarytool.gradle
 
+import java.nio.file.Path
+
 /**
  * Reads the text from the resource file and returns it.
  *
@@ -7,4 +9,13 @@ package ca.ewert.notarytool.gradle
  */
 public fun readBuildFileContents(resource: String): String {
   return object {}.javaClass.getResource(resource)?.readText(Charsets.UTF_8) ?: ""
+}
+
+/**
+ * Convenience Method to get a [Path] from a resource location.
+ *
+ * @param resource Name of the resource (not must start with `/`
+ */
+internal fun resourceToPath(resource: String): Path? {
+  return object {}.javaClass.getResource(resource)?.toURI()?.let { Path.of(it) }
 }
